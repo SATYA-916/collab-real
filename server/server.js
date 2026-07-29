6b0e48f23803dd8c94fcb2243162bd2baa0990a6
SATYA-916 <satyasampath79@gmail.com>
2026-07-29 23:54:04 +0530

    Remove Docker/K8s/AWS files, keep pure MERN stack

--- server/server.js
+++ server/server.js
@@ -30,7 +30,6 @@ app.get('/health', (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 3000
-httpServer.listen(PORT, () => {
-    console.log("Server is running on port " + PORT)
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
