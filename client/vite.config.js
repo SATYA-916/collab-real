b4faa580c9b52a67c883fb842ccedd926124edd5
SATYA-916 <satyasampath79@gmail.com>
2026-07-30 00:05:26 +0530

    Add file creation and language selection buttons

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ react(), tailwindcss() ],
+  resolve: {
+    alias: {
+      'monaco-editor/esm/vs/editor/editor.api.js':
+        path.resolve(__dirname, 'node_modules/monaco-editor/esm/vs/editor/editor.api.js'),
+    },
+  },
+  optimizeDeps: {
+    include: ['monaco-editor'],
+  },
 })
